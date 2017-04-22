1f21c472ef3c924f65db499e185e38dda6e1f453
Nicholas Hubbard <nrhubbar@iupui.edu>
2017-04-22 15:00:47 -0400

    Added face finding and implemented saving found faces

--- main.py
+++ main.py
@@ -5,15 +5,11 @@ def find_faces():
     image = cv2.imread("3-faces.jpg")
     faceCascade = cv2.CascadeClassifier("haarcascade_profileface.xml")
     grayimage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(
-        grayimage,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30)
-    )
+    faces = faceCascade.detectMultiScale(grayimage)
 
     for (x, y, w, h) in faces:
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        subface = image[y:y+h, x:x+w]
+        cv2.imwrite("faces_{}.jpeg".format(y), subface)
 
     cv2.imshow("Faces found", image)
     cv2.waitKey(0)
