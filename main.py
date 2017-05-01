83217ec86c96df273cd1b64708642f38f64fb721
Nicholas Hubbard <nrhubbar@iupui.edu>
2017-05-01 13:05:53 -0400

    Changed function for type convert

--- main.py
+++ main.py
@@ -95,8 +95,8 @@ def feature_match(images):
     kp1, des1 = sift.detectAndCompute(images[0], None)
     kp2, des2 = sift.detectAndCompute(images[1], None)
 
-    des1.convert_to(des1, cv2.CV_32F)
-    des2.convert_to(des2, cv2.CV_32F)
+    des1 = des1.astype(cv2.CV_32F)
+    des2 = des2.astype(cv2.CV_32F)
 
     FLANN_INDEX_KDTREE = 1
     index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
